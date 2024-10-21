70c27fadc33f287b32765b041d9aafc4b3e8de31
Kathryn Mercier <mercier.kate@gmail.com>
2024-10-21 11:18:40 -0400

    Update vsicurl_notes.R

--- data-raw/chelsa/vsicurl_notes.R
+++ data-raw/chelsa/vsicurl_notes.R
@@ -1,4 +1,4 @@
-foo2 <- terra::rast("/vsicurl/https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-101_V1.0.tif ")
+foo2 <- terra::rast("/vsicurl/https://os.zhdk.cloud.switch.ch/chelsav1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-101_V1.0.tif ")
 e <- ext(-5, 5, -5, 5)
 foo2 <- crop(foo2,e)
 foo3<-aggregate(foo2,50)
@@ -6,9 +6,9 @@ foo3<-aggregate(foo2,50)
 # use orog to mask ocean
 # do we have ice as well?
 
-tiff_url <- c ("/vsicurl/https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-101_V1.0.tif",
-   "/vsicurl/https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-102_V1.0.tif", 
-   "/vsicurl/https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-103_V1.0.tif")
+tiff_url <- c ("/vsicurl/https://os.zhdk.cloud.switch.ch/chelsav1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-101_V1.0.tif",
+   "/vsicurl/https://os.zhdk.cloud.switch.ch/chelsav1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-102_V1.0.tif", 
+   "/vsicurl/https://os.zhdk.cloud.switch.ch/chelsav1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-103_V1.0.tif")
 foo2<-terra::rast(tiff_url)
 coords<-data.frame(longitude = c(5, -4, 27, -6, -24), latitude = c(7, 44, 36, 56, 31))
 extract(foo2,coords)
@@ -28,7 +28,7 @@ plot(foo)
 ##
 # cloud optimised geotiff (not available for bio variables yet)
 describe("/vsicurl/https://os.zhdk.cloud.switch.ch/chelsa01/chelsa_trace21k/global/centennial/pr/CHELSA_TraCE21k_pr_01_-200_V.1.0.tif")
-describe("/vsicurl/https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-101_V1.0.tif")
+describe("/vsicurl/https://os.zhdk.cloud.switch.ch/chelsav1/chelsa_trace/bio/CHELSA_TraCE21k_bio01_-101_V1.0.tif")
 
 ## Modern chelsa with virtual dataset
 chelsa_present_bio_url <- paste0("/vsicurl/https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V2/GLOBAL/climatologies/1981-2010/bio/CHELSA_bio",1:19,"_1981-2010_V.2.1.tif")
